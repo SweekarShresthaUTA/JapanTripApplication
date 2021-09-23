bd2ebb36164a726b49459afeab17c0cfe384a39f
Shrestha <sweekarstha10@gmail.com>
2021-09-23 13:22:44 -0500

    implemented the routing for the currency exchange

---
+++ my-app/src/Components/ResourcesComponents/CurrencyExchange.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+
+function currencyExchange() {
+    console.log("entered in currency exchange");
+    return(
+        <div>
+            CurrencyExchange
+        </div>
+    );
+
+}
+
+export default currencyExchange;
