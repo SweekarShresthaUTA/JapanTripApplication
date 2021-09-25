7dabdaedaa0e8ccb2ff5416e2a99e6b198e742e7
Ethan Nguyen <ethan.nguyen@mavs.uta.edu>
2021-09-25 15:38:02 -0500

    adding images

--- my-app/src/Components/ScheduleComponents/Schedule.jsx
+++ my-app/src/Components/ScheduleComponents/Schedule.jsx
@@ -15,6 +15,24 @@ const Schedule = () => {
                 thirdDropDownMenuOption="Cultural Information"
             />
             <h1>Schedule</h1>
+
+            <img src="9-13-page-000.jpg" width="550" height="700" alt="Tokyo"></img>
+            <br/>
+
+            <img src="9-13-page-001.jpg" width="550" height="700" alt="Tokyo"></img>
+            <br/>
+
+            <img src="9-13-page-002.jpg" width="550" height="700" alt="Tokyo"></img>
+            <br/>
+
+            <img src="9-13-page-003.jpg" width="550" height="700" alt="Tokyo"></img>
+            <br/>
+
+            <img src="9-13-page-004.jpg" width="550" height="700" alt="Tokyo"></img>
+            <br/>
+            
+            <img src="9-13-page-005.jpg" width="550" height="700" alt="Tokyo"></img>
+            
         </div>
     );
 }
